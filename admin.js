02cf1a141aff6873ac7ecd8d14418ea2652e7a5e
Ahmer Malik <ahmer9800@gmail.com>
2017-07-31 23:38:37 -0500

    Test One

---
+++ admin.js
@@ -0,0 +1,31 @@
+/**
+ * Created by ahmermalik on 7/31/17.
+ */
+
+
+var creditPrice = 1.98;
+var cashBack = .97;
+var gallons = 17;
+var cashPrice = 1.94;
+
+savings = "The savings per gallon if you use a credit card is " + creditPrice * cashBack;
+creditOnly = "The savings per full tank of gas if you use a credit card is " +creditPrice * cashBack * gallons;
+cashOnly = "If you paid with cash, it would cost " +cashPrice *gallons;
+
+
+
+console.log(savings);
+console.log(cashOnly);
+
+console.log(creditOnly)
+
+if (creditOnly > cashOnly) {
+        result = "More savings if you pay using cash";
+    }
+
+else {
+    result = "More savings if you pay using credit";
+}
+
+     console.log(result);
+
